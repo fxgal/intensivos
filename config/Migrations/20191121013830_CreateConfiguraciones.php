a3243a56a868bcc1ae884a9da74c7e008e88c4ba
Ris3TwO <ris3two@gmail.com>
2019-11-22 10:37:25 -0400

    Migración de Personas y Configuraciones creada

---
+++ config/Migrations/20191121013830_CreateConfiguraciones.php
@@ -0,0 +1,41 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateConfiguraciones extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('configuraciones');
+        $table->addColumn('nombre', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('clave', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('valor', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
