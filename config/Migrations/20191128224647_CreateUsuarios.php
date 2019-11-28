e89cabd10eb35f817d833db8fa306d3420ac00db
Ris3TwO <ris3two@gmail.com>
2019-11-28 18:47:26 -0400

    Migración de usuario agregada

---
+++ config/Migrations/20191128224647_CreateUsuarios.php
@@ -0,0 +1,47 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsuarios extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('usuarios');
+        $table->addColumn('rol_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('username', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('password', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addIndex([
+            'username',
+        ], [
+            'name' => 'UNIQUE_USERNAME',
+            'unique' => true,
+        ]);
+        $table->create();
+    }
+}
