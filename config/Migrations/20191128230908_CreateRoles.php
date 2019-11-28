ffbc0c8bb5b94705053b09d388dd37564ec6af4a
Ris3TwO <ris3two@gmail.com>
2019-11-28 19:09:23 -0400

    Migración de roles agregada

---
+++ config/Migrations/20191128230908_CreateRoles.php
@@ -0,0 +1,28 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateRoles extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('roles');
+        $table->addColumn('nombre', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('codigo', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
