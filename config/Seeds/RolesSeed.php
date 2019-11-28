a21659c884d1b74b7b71a158a453f38f6012144b
Ris3TwO <ris3two@gmail.com>
2019-11-28 19:35:23 -0400

    Añadido seed de roles

---
+++ config/Seeds/RolesSeed.php
@@ -0,0 +1,42 @@
+<?php
+
+use Migrations\AbstractSeed;
+
+/**
+ * Roles seed.
+ */
+class RolesSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->execute('TRUNCATE TABLE roles');
+
+        $data = [];
+
+        $data = [
+            [
+                'nombre'        => "Administrador",
+                'codigo'        => "admin",
+                'created'       => date('Y-m-d H:i:s'),
+            ],
+            [
+                'nombre'        => "Estudiante Regular",
+                'codigo'        => "estudiante",
+                'created'       => date('Y-m-d H:i:s'),
+            ]
+        ];
+
+        $table = $this->table('roles');
+        $table->insert($data)->save();
+    }
+}
