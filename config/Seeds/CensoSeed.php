186f4384c071898867d62bbc4dbd3fec76f78142
Ris3TwO <ris3two@gmail.com>
2020-02-13 00:19:39 -0400

    Seed de pcenso añadido

--- config/Seeds/CensoSeed.php
+++ config/Seeds/CensoSeed.php
@@ -18,20 +18,22 @@ class CensoSeed extends AbstractSeed
      */
     public function run()
     {
-        // $faker = Faker\Factory::create();
-        // $data = [];
+        $faker = Faker\Factory::create();
+        $data = [];
 
-        // for ($i = 0; $i < 400; $i++) {
-        //     $data = [
-        //         'persona_id'    => ($i + 1),
-        //         'materia_id'    => ($i + 1),
-        //         'periodo_id'    => $faker->randomElement(1, 2),
-        //         'modified'      => date('Y-m-d H:i:s'),
-        //         'created'       => date('Y-m-d H:i:s'),
-        //     ];
-        // }
+        for ($i = 0; $i < 400; $i++) {
+            for ($j = 0; $j < 2; $j++){
+                $data[] = [
+                    'persona_id'    => ($i + 1),
+                    'materia_id'    => $faker->numberBetween(1, 58),
+                    'periodo_id'    => $faker->numberBetween(1, 2),
+                    'modified'      => date('Y-m-d H:i:s'),
+                    'created'       => date('Y-m-d H:i:s'),
+                ];
+            }
+        }
 
-        // $table = $this->table('censos');
-        // $table->insert($data)->save();
+        $table = $this->table('censos');
+        $table->insert($data)->save();
     }
 }
